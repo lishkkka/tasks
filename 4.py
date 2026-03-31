d9ae0fa28d8354ef8f2084400331258aae64a191
Lisa <elisabelyaevageo@gmail.com>
2026-03-31 16:45:38 +0300

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,12 @@
+# 04
+name = "Lisa"
+group = "MiddleB"
+age = 19
+city = "Harbin"
+b = "+" + "-" * 30 + "+"
+print(b)
+print("| NAME :", name, "                 |")
+print("| GROUP:", group, "              |")
+print("| AGE  :", age, "                   |")
+print("| CITY :", city, "               |")
+print(b)
---
+++ 5.py
@@ -0,0 +1,5 @@
+drive = "D:"
+folders = "Projects"
+sub = "python"
+file = "XXX.py"
+print(drive, folders, sub, file, sep = "\\")
