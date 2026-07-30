e86de35cc6b10e5279278141ef96c2d44ac8db2c
Lisa <elisabelyaevageo@gmail.com>
2026-07-30 14:55:33 +0300

    Add files via upload

--- 100.py
+++ 100.py
@@ -1,20 +1,26 @@
 N = int(input("Введите количество элементов: "))
 a = list(map(int, input("Введите элементы списка: ").split()))
-flag = 0
-for x in range(-1, N):
-    b = []
-    for i in range(N):
-        if i != x:
-            b.append(a[i])
-    check = 1
-    for i in range(1, len(b) - 1):
-        if b[i] < b[i - 1] and b[i] < b[i + 1]:
-            check = 0
-        if b[i] > b[i - 1] and b[i] > b[i + 1]:
-            check = 0
-    if check == 1:
-        flag = 1
-if flag == 1:
+
+
+def stable(arr):
+    if len(arr) <= 2:
+        return True
+    for i in range(1, len(arr) - 1):
+        if not (min(arr[i - 1], arr[i + 1]) <= arr[i] <= max(arr[i - 1], arr[i + 1])):
+            return False
+    return True
+
+if stable(a):
     print("Да")
 else:
-    print("Нет")
+    f = 0
+    for i in range(N):
+        new_arr = a[:i] + a[i + 1:]
+        if stable(new_arr):
+            f= 1
+            break
+
+    if f == 1:
+        print("Да")
+    else:
+        print("Нет")
